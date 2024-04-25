9ccf4fe99f6c003e82c399241a95ab94aa6d37e4
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2024-04-25 16:34:54 +0300

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> input = new SimpleStack<>();
+    private final SimpleStack<T> output = new SimpleStack<>();
+
+    private int intSize;
+    private int outSize;
+
+    public T poll() {
+        if (intSize == 0 && outSize == 0) {
+            throw new NoSuchElementException("Queue is empty");
+        }
+        for (int i = 0; i < intSize; i++) {
+            if (outSize != 0) {
+                input.push(output.pop());
+            }
+            output.push(input.pop());
+        }
+        outSize = intSize;
+        intSize = 0;
+        outSize--;
+        return output.pop();
+    }
+
+    public void push(T value) {
+        input.push(value);
+        intSize++;
+    }
+}
