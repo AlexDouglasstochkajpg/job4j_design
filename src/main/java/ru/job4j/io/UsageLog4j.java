f06619eaf71e404b1d52b3069badf8dad6213fab
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2024-06-26 13:32:05 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,19 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Petr Arsentev";
+        int age = 33;
+        LOG.debug("User info name : {}, age : {}", name, age);
+
+        byte newByte = 1;
+        short newShort = 150;
+        int newInt = 150000;
+        long newLong = 10000000000L;
+        float newFloat = 10.5F;
+        double newDouble = 10.5;
+        char newChar = 'A';
+        boolean newBoolean = true;
+        LOG.debug("Byte: {}, short: {}, int: {}, long: {}, float: {}, double: {}, char: {}, boolean: {}",
+                newByte, newShort, newInt, newLong, newFloat, newDouble, newChar, newBoolean);
     }
 }
