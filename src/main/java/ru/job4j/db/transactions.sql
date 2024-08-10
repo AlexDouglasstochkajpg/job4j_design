e81b9feb122138f8d7492a27b1e21765ddb5baf9
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2024-08-10 18:13:50 +0300

    4.3. Транзакции. Уровни изолированности транзакций. [#504812]

---
+++ src/main/java/ru/job4j/db/transactions.sql
@@ -0,0 +1,13 @@
+create table cars(
+	id serial primary key,
+	name text,
+	price integer,
+	transmission text
+);
+
+insert into cars(name, price, transmission)
+values('BMW', 5000000, 'automatic');
+insert into cars(name, price, transmission)
+values('Lada', 1000000, 'mechanic');
+insert into cars(name, price, transmission)
+values('Porsche', 10000000, 'robot');
