01e259b4e60c6f29787e0fc5ea2dce37a14612b8
AlexDouglasstochkajpg <domnitzkij.alexander@yandex.ru>
2024-11-25 17:00:58 +0300

    2. Парковка машин [#853]

---
+++ src/main/java/ru/job4j/ood/parking/park/Parking.java
@@ -0,0 +1,6 @@
+package ru.job4j.ood.parking.park;
+public class Parking extends AbstractParking {
+    public Parking(int sizeForPassenger, int sizeForCargo) {
+        super(sizeForPassenger, sizeForCargo);
+    }
+}
